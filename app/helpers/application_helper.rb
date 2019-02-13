57963b2f09bbc8595bb991bd185b72e841307f17
devlucasrocha <lucasgeorgerocha@gmail.com>
2019-02-13 12:24:41 -0200

    Modifying somethings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,17 @@
 module ApplicationHelper
     def toArray(word)
         word_array = word.upcase.split('')
-        rand_letters = ((0...3).map { (65 + rand(26)).chr }.join).split('')
+        rand_letters = ((0...10).map { randLetter(word_array) }.join).split('')
         word_array += rand_letters
         word_string = word.upcase
         return [word_array.shuffle!, word_string]
+    end
+
+    def randLetter(word_array)
+        loop do
+            gene = (65 + rand(26)).chr
+            check = word_array.include?(gene)
+            return gene if !check
+        end
     end
 end
