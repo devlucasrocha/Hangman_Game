98a8b4a750b494db5798ac6e342452c0885f36b9
devlucasrocha <lucasgeorgerocha@gmail.com>
2019-02-13 01:43:57 -0200

    Seeing if the value of  button that got clicked is in the secret word

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
     def toArray(word)
-        word.upcase.split('')
+        word_array = word.upcase.split('')
+        rand_letters = ((0...3).map { (65 + rand(26)).chr }.join).split('')
+        word_array += rand_letters
+        word_string = word.upcase
+        return [word_array.shuffle!, word_string]
     end
 end
