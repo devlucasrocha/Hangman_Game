f24aa56518e7a54da076ce1f93adf1eb7a7e239f
devlucasrocha <lucasgeorgerocha@gmail.com>
2019-02-14 15:25:14 -0200

    Creating an scaffold Word and showing 1 word on the view thru controller

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,4 +1,11 @@
 class GameController < ApplicationController
   def main_page
+    @words = Word.find(randomId())
+  end
+
+  private
+  def randomId
+    r = Random.new
+    r.rand(1...Word.all.size + 1)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :words
   get 'game/main_page'
   root to: 'game#main_page'
   get 'main', to: 'game#main_page'
