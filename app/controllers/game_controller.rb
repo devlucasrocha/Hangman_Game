2a09028ab60e98a5419373523740f9e5103c072a
devlucasrocha <lucasgeorgerocha@gmail.com>
2019-02-16 00:43:24 -0200

    Improving game_controller

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,11 +1,16 @@
 class GameController < ApplicationController
   def main_page
     @words = Word.find(randomId())
+    showRandomWord(@words.word)
   end
 
   private
   def randomId
     r = Random.new
     r.rand(1...Word.all.size + 1)
+  end
+
+  def showRandomWord word
+    puts "\033[1;31m\n\n\n\n\t\t\t################# SECRET WORD -> #{word} #################\n\n\n\n\033[m"
   end
 end
