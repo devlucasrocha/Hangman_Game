b57b9a8952b07a390a45806394434fb80be68737
devlucasrocha <lucasgeorgerocha@gmail.com>
2019-02-15 22:57:44 -0200

    Improving the helper that returns arrays containing random letters

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,31 @@
 module ApplicationHelper
     def toArray(word)
-        word_array = word.upcase.split('')
-        rand_letters = ((0...12).map { randLetter(word_array) }.join).split('')
-        word_array += rand_letters
+        word_array = word.upcase
+        rand_letters = randLetter(word)
+        word_array = rand_letters
         word_string = word.upcase
         return [word_array.shuffle!, word_string]
     end
-
+    
     def randLetter(word_array)
-        loop do
-            gene = (65 + rand(26)).chr
-            check = word_array.include?(gene)
-            return gene if !check
+        letters = []
+        (0...5).each do |e|
+            gene = (0...8).map { (65 + rand(26)).chr }.join
+            gene = gene.split('')
+            gene.each do |l|
+                if (letters.include?(l))
+                    next
+                end
+                letters << l
+            end
         end
+        word_array = word_array.split('')
+        word_array.each do |l|
+            if (!letters.include?(l))
+                letters << l
+            end
+        end
+        letters.shuffle!
     end
 
     def colorBody
